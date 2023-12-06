c916e1251eb9f9ca0da9ea5b0a9eea1726fb433d
Byron Hannaford <byron.a.hannaford@gmail.com>
2023-12-06 22:00:00 +0000

    Day 1 Part 2

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 
 <!--SOLUTIONS-->
 
-[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%86/yellow)](src/day01)
+[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 ![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
@@ -69,9 +69,9 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 14.503ms
-Time part 2: -
-Both parts: 14.503ms
+Time part 1: 2.532ms
+Time part 2: 32.767ms
+Both parts: 35.299ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 1/50
-Total time: 14.503ms
+Total stars: 2/50
+Total time: 35.299ms
 ```
 
 <!--/RESULTS-->
--- src/day01/index.ts
+++ src/day01/index.ts
@@ -22,9 +22,41 @@ const part1 = (rawInput: string) => {
 };
 
 const part2 = (rawInput: string) => {
+  const numberDict: { [key: string]: number } = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
   const input = parseInput(rawInput);
 
-  return;
+  const mappedInput = input.split("\n").map((input) => {
+    const numList: Array<Number> = [];
+
+    input.split("").forEach((letter, index) => {
+      if (!isNaN(+letter)) {
+        numList.push(+letter);
+      } else {
+        Object.entries(numberDict).forEach(([key, value]) => {
+          if (input.substring(index, index + key.length) === key) {
+            numList.push(value);
+          }
+        });
+      }
+    });
+
+    return +`${numList[0]}${numList.at(-1)}`;
+  });
+
+  return mappedInput.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
 };
 
 run({
